559d9750397ca142fc00ae8a16a30eaf92938530
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2016-05-19 16:40:55 -0400

    made transition to second page cool

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("connected to app.js");
+
+$("#get-started").on("click", function(){
+  $("#main").fadeOut("slow", function(){
+    console.log("get-started animation complete");
+    $("#explore").fadeIn("slow");
+  });
+});
